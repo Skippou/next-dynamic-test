69c2dd5c5865b03aa5b0c43bc04fd102d6f3eb78
Gerrit Cloete <gerritcloete@gmail.com>
2023-11-10 16:03:49 +0200

    Testing the dynamic routes

---
+++ next-dynamic-test/app/project/[projectId]/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { useParams } from "next/navigation";
+interface PageProps {
+  params: { projectId: string };
+}
+export default function Page({ params }: PageProps) {
+    const useThisParam = useParams<{ projectId: string }>();
+  return <p className="text-white bg-black">TESTING{useThisParam.projectId } {params?.projectId} Hi</p>;
+}
+export const dynamicParams = true
+export const revalidate = false
+export const dynamic = 'force-dynamic'
